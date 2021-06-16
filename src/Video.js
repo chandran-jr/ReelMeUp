a18ab08616cbe6f0c81f58c3801977aa3107c396
Govind B Chandran <govindchandran235@gmail.com>
2021-06-16 20:59:07 +0530

    Add files via upload

---
+++ src/Video.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Video.css';
+
+function Video() {
+    return (
+        <div className="video">
+            
+        </div>
+    )
+}
+
+export default Video
