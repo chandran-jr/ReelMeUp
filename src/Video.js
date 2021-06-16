0c7afa4caf34ec5962ee38b9880d35bed4e27daf
Govind B Chandran <govindchandran235@gmail.com>
2021-06-16 23:27:07 +0530

    Add files via upload

--- src/Video.js
+++ src/Video.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './Video.css';
 
 function Video() {
+
     return (
         <div className="video">
-        <iframe className="video__player" width="100%" height="100%" src="https://www.youtube.com/embed/TzKy1cMmvps" title="Reels" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe className="video__player" loop width="100%" height="100%" src="https://www.youtube.com/embed/TzKy1cMmvps" title="Reels" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             
         </div>
     )
