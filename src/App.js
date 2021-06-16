0fc57684abaca14d4cf0ef982d98ad7da8578a4d
Govind B Chandran <govindchandran235@gmail.com>
2021-06-16 20:57:37 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import React from 'react';
 import './App.css';
+import Video from './Video';
 
 function App() {
   return (
     <div className="App">
-      
+
+
+    <div className="app__videos">
+
+    </div>
+      <Video/>
     </div>
   );
 }
