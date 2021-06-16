982de079ce49784d66b621760dee1b727fdc8625
Govind B Chandran <govindchandran235@gmail.com>
2021-06-16 20:11:02 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDtWUKYuG_LuE1_nyruq1mSM2gqPwHLKk0",
+    authDomain: "reelmeup-9a302.firebaseapp.com",
+    projectId: "reelmeup-9a302",
+    storageBucket: "reelmeup-9a302.appspot.com",
+    messagingSenderId: "866189862352",
+    appId: "1:866189862352:web:d448f8503bc31dcea8c5d5"
+  };
